861e2a8adf65fabab05d5d14457108c3a43792f5
neliza <nicolasaelf64@gmail.com>
2017-10-05 18:32:11 -0600

    beginning inquirer

--- game.js
+++ game.js
@@ -1,3 +1,24 @@
 var inquirer = require('inquirer');
-var constructorFunctions = require('./constructorFunctions.js');
+var constructorFunctions = require('./cardFunctions.js');
 var fs = require ("fs");
+
+inquirer.prompt([
+    {
+        type: "list",
+        message: "Choose one!",
+        choices: [  "Create a basic flashcard",
+                    "Create a cloze flashcard",
+                    "Play flashcard game from built card array",
+                    "Quit Game"],
+        name: "flashcards",
+    }           
+]).then(function(optionsResponse){
+    if(optionsResponse.flashcards === "Create a basic flashcard") {
+        console.log("You want to create a basic flashcard");
+    } else if (optionsResponse.flashcards === "Create a cloze flashcard"){
+        console.log("You want to create a cloze flashcard");
+    }
+
+
+
+})
